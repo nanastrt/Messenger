10bab13286cd8281c6327951ec0df549514a42bd
Heimaidi <125238699+nanastrt@users.noreply.github.com>
2023-11-09 03:54:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 #define
-define('API_KEY','TOKEN'); //
+define('API_KEY','TOKEN');
 #function
 function bot($method,$datas=[]){
     $url = 'https://api.telegram.org/bot'.API_KEY.'/'.$method;
